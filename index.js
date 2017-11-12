b6295f2ec583b5d2f9f1039e1c0a0a7ba1ba3462
luxxxxxx <Always-Luck@qq.com>
2017-11-12 20:50:49 +0800

    修改项目结构  准备着手后台开发

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require("http"),
+    express = require("express"),
+    bodyParser = require("body-parser"),
+    cookieParser = require("cookie-parser"),
+    session = require("express-session"),
+    app = express();
+
+app.set('views',__dirname+'/views');// 设置模板引擎的目录
+app.set('view engine','ejs');// 设置使用的模板引擎是什么
+app.use(express.static(__dirname+'/public'));// 设置静态资源目录 js img  css
+
+app.use(bodyParser.json());// 用来接收json的数据
+// extended:true 可以接收任何数据类型的数据
+app.use(bodyParser.urlencoded( { extended:true } ));
+app.use(cookieParser("luxxxxxx"));
+app.use(session( { secret : "luxxxxxx" } ));  //设置秘钥
+
+
+// app.use((req,res,next) => {  //全局cookie
+//     if (req.cookies["login"]) {
+//     res.locals.login = req.cookies.login.name;
+// }
+// next();
+// });
+
+app.use('/',require('./router/index'));
+
+
+//  http://localhost:233  /admim 这个路径已经被app.use匹配了
+//        /index 这个路径是来交给admin.js文件进行匹配的
+//app.use('/admin',require('./router/admin'));
+
+http.createServer(app).listen(233);
---
+++ router/index.js
@@ -0,0 +1,7 @@
+const express = require("express"),
+    // mysql = require("../module/mysql"),
+    crypto = require("crypto"),
+    router = express.Router();
+
+
+module.exports = router;
