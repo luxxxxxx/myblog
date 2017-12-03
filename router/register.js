4f5928466e041c08fb3fcf35e8b4473dd22a0091
luxxxxxx <Always-Luck@qq.com>
2017-12-03 20:56:14 +0800

    fixed

--- router/register.js
+++ router/register.js
@@ -1,5 +1,5 @@
 const express = require("express"),
-    // mysql = require("../module/mysql"),
+    mysql = require("../module/mysql"),
     crypto = require("crypto"),
     send = require("../module/sendMail"),
     router = express.Router();
@@ -8,6 +8,18 @@ router.get("/",(req,res) => {
     res.render("./reg.ejs")
 });
 
+let decode = (str) => {  //解密
+    let deCodeStr = decodeURIComponent(str),
+        arr = [];
+        for (let i = 0; i < deCodeStr.length ; i ++) {
+            arr[i] = deCodeStr.charCodeAt(i) - i;
+        }
+        let string = '';
+        for (let i in arr) {
+            string += String.fromCharCode(arr[i])
+        }
+        return string;
+};
 router.get("/checkCode",(req,res) => {
     res.send("gg");
     let userName =  req.query.userName
@@ -18,7 +30,6 @@ router.post('/sendMail',(req,res) => {
         for (var i = 0; i < str.length; i++) {
             arr[i] = str.charCodeAt(i) + i;
         }
-        console.log(arr)
         let string = '';
         for (let j in arr) {
             string += String.fromCharCode(arr[j]);
@@ -27,6 +38,7 @@ router.post('/sendMail',(req,res) => {
         return encodeStr;
     },
     activeEmail = encode(req.body.email),
+
     mail = {
         from : "luxxxxxx <wy981236133@126.com>",
         subject : "测试2",
@@ -35,27 +47,54 @@ router.post('/sendMail',(req,res) => {
         html : "欢迎来到要优雅官方注册页面，请在三分钟内点击以下链接，以此来完成邮箱校检.</br> <a>localhost:233/reg/activeEmail?email="+ activeEmail +"</a>"
     };
     console.log(req.body.email);
-    console.log(activeEmail);
-    send(mail);
-});
-router.get("/activeEmail",(req,res) => {
-    let decode = (str) => {
-        let deCodeStr = decodeURIComponent(str),
-            arr = [];
-        console.log('decodeURIComponent=' + deCodeStr);
-        for (let i = 0; i < deCodeStr.length ; i ++) {
-            arr[i] = deCodeStr.charCodeAt(i) - i;
-        }
-        let string = '';
-        for (let i in arr) {
-            string += String.fromCharCode(arr[i])
-        }
-        return string;
-    },
+    mysql({
+        sql : 'insert into t_user_email (email,isActive) values (?,0)',
+        args : [req.body.email],
+        callback : function (info) {
+            if (info) {
+                //这里需要验证 邮箱是否已经被激活以及使用, sql 需要判断 是否 输入的email 在 user_email 表里面，如果在 （err）情况 就 不执行数据库操作 ,直接发送邮件.
+                res.send('数据库表email嵌入发生错误  -- 56\n' + info ); //Duplicate  重复
 
-    email = decode(req.query.email);
+            } else {
+                send(mail);
+            }
+        }
+    })
+});
+
+router.get("/activeEmail",(req,res) => {
+    let email = decode(req.query.email);
+    mysql({
+        sql : 'select * from t_user_email where email = ?',
+        args : [email],
+        callback(err,data) {
+            console.log(err);
+            console.log(data);
+            if (!err) {  //null 成功
+                console.log(data[0].isActive);
+                if (!data[0].isActive) {
+                    mysql({
+                        sql : 'update t_user_email set isActive = 1 where t_user_email.email = ?',
+                        args : [email],
+                        callback(err,info) {
+                            if (err) {  //失败
+                                res.render('email_callback.ejs')
+                            } else {
+                                res.render('email_callback.ejs');
+                            }
+                        }
+                    })
+                } else {
+                    res.render('email_callback.ejs');
+                }
+            } else {
+                console.log(err);
+            }
+        }
+    })
+
     console.log(decode(req.query.email));
-    res.render('activeEmailSuccess.ejs');
+    res.render('email_callback.ejs');
 })
 
 
