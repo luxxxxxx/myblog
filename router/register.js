24f934c9e4ad75df672250353b2306260fc81737
luxxxxxx <Always-Luck@qq.com>
2017-12-04 17:10:45 +0800

    update register page

--- router/register.js
+++ router/register.js
@@ -44,7 +44,7 @@ router.post('/sendMail',(req,res) => {
         subject : "测试2",
         // to : req.body["email"],
         to : "981236133@qq.com",
-        html : "欢迎来到要优雅官方注册页面，请在三分钟内点击以下链接，以此来完成邮箱校检.</br> <a>localhost:233/reg/activeEmail?email="+ activeEmail +"</a>"
+        html : "欢迎来到要优雅官方注册页面，点击以下链接，以此来完成邮箱校检.</br> <a>localhost:233/reg/activeEmail?email="+ activeEmail +"</a>"
     };
     console.log(req.body.email);
     mysql({
@@ -53,7 +53,12 @@ router.post('/sendMail',(req,res) => {
         callback : function (info) {
             if (info) {
                 //这里需要验证 邮箱是否已经被激活以及使用, sql 需要判断 是否 输入的email 在 user_email 表里面，如果在 （err）情况 就 不执行数据库操作 ,直接发送邮件.
-                res.send('数据库表email嵌入发生错误  -- 56\n' + info ); //Duplicate  重复
+                // res.send('数据库表email嵌入发生错误  -- 56\n' + info ); //Duplicate  重复
+                if (/\bDuplicate\b/.test(info)) {
+                    send(mail);
+                } else {
+                    res.send('err');
+                }
 
             } else {
                 send(mail);
@@ -71,30 +76,28 @@ router.get("/activeEmail",(req,res) => {
             console.log(err);
             console.log(data);
             if (!err) {  //null 成功
-                console.log(data[0].isActive);
                 if (!data[0].isActive) {
                     mysql({
                         sql : 'update t_user_email set isActive = 1 where t_user_email.email = ?',
                         args : [email],
                         callback(err,info) {
                             if (err) {  //失败
-                                res.render('email_callback.ejs')
+                                res.render('email_callback.ejs',{data : '更新数据库发生未知错误，请联系网站管理员email:981236133@qq.com'})
                             } else {
-                                res.render('email_callback.ejs');
+                                res.render('email_callback.ejs',{data : '恭喜！你的邮箱激活成功!请返回注册界面完成注册~'});
                             }
                         }
                     })
                 } else {
-                    res.render('email_callback.ejs');
+                    res.render('email_callback.ejs',{data : '你的邮箱已经处于激活状态~'});
+
                 }
             } else {
+                console.log('err+++');
                 console.log(err);
             }
         }
     })
-
-    console.log(decode(req.query.email));
-    res.render('email_callback.ejs');
 })
 
 
