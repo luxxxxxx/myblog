a2dd5560ccca6bf5d2415aadfada66a002e4824e
luxxxxxx <Always-Luck@qq.com>
2017-12-21 23:24:58 +0800

    test

--- router/register.js
+++ router/register.js
@@ -94,6 +94,7 @@ router.get("/activeEmail",(req,res) => {
 })
 router.post ('/vertifyUserName',(req,res) => {
     let userName = req.body.userName;
+    console.log(userName);
     mysql({
         sql : 'select * from t_user where user_name = ?',
         args : [userName],
