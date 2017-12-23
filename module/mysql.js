d9ddf11816b232f7b158c907b7de73185949b589
luxxxxxx <Always-Luck@qq.com>
2017-12-23 18:20:23 +0800

    test

--- module/mysql.js
+++ module/mysql.js
@@ -1,20 +1,15 @@
 const mysql = require("mysql");
 module.exports = (obj) => {
     // obj 参数：sql，args：查询数据 （例username）的数组形式，callback：回调函数
+    
     let config = mysql.createConnection({
         host: "127.0.0.1",
-        port : "3306",
-        user : "root",
-        password : "lux5001131",
-        database : "blog"
+        port: "3306",
+        user: "root",
+        password: "",
+        // password: "lux5001131",
+        database: "blog"
     })
-    // let config = mysql.createConnection({
-    //     host: "127.0.0.1",
-    //     port: "3306",
-    //     user: "root",
-    //     password: "",
-    //     database: "blog"
-    // })
     config.connect();
     if (!obj.args) {
         config.query(obj.sql,(err,data)=>{
