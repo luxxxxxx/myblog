4a38e8fbb27aaafcae6dae24ede0b2404ec4a857
luxxxxxx <Always-Luck@qq.com>
2017-12-24 21:31:46 +0800

    修改了发送弹幕bug，实现了只有登录才能发送弹幕功能

--- index.js
+++ index.js
@@ -32,24 +32,6 @@ app.use(session({
 
 
 
-// const cipher = crypto.createCipher('aes192', 'a password');
-
-// let encrypted = '';
-// cipher.on('readable', () => {
-//     const data = cipher.read();
-//     if (data)
-//         encrypted += data.toString('hex');
-// });
-// cipher.on('end', () => {
-//     console.log(encrypted);
-//     // Prints: ca981be48e90867604588e75d04feabb63cc007a8f8ad89b10616ed84d815504
-// });
-
-// cipher.write('some clear text data');
-// cipher.end();
-
-
-
 
 app.use ((req,res,next) => {
     
