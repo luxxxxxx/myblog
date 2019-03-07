c5cec5590a1cd465ec3e2ff44def7a7d472213e2
clceY <Always-Luck@qq.com>
2019-03-07 19:00:05 +0800

    forget change the pass

--- module/mysql.js
+++ module/mysql.js
@@ -5,8 +5,8 @@ module.exports = (obj) => {
         host: "127.0.0.1",
         port: "3306",
         user: "root",
-        password: "",
-        // password: "liu5001131",
+        // password: "",
+        password: "liu5001131",
         // last server pass: "lux5001131",
         database: "blog"
     })
